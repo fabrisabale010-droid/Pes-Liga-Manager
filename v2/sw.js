49ec9ca6245079ac0e9819572e8bb72aff058b5c
fabrisabale010-droid <fabrisabale010@gmail.com>
2026-09-02 22:49:06 -0300

    Add files via upload

--- v2/sw.js
+++ v2/sw.js
@@ -1,7 +1,7 @@
 /* Guarda la app en el teléfono para que abra aunque no haya señal.
    Los datos siguen viniendo de Firebase cuando hay conexión. */
 
-const CACHE = 'pes6-v2-8';
+const CACHE = 'pes6-v2-9';
 
 const SHELL = [
   './',
